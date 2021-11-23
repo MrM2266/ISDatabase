de412420b0d0cfdc16d6f28cb542c57b3cf64344
MrM2266 <75018273+MrM2266@users.noreply.github.com>
2021-11-23 22:06:25 +0100

    Added new relationships

--- model.py
+++ model.py
@@ -9,25 +9,30 @@ engine = create_engine('sqlite:///code.db', echo = True)
 Session = sessionmaker(bind = engine)
 Base = declarative_base()
 
+
+
+
 person_teaches = Table("person_lesson", Base.metadata,
                         Column("person_id", Integer, ForeignKey('people.id')),
                         Column("lesson_id", Integer, ForeignKey('lessons.id')))
 
-class person(Base):
+class Person(Base):
     __tablename__ = "people"
     
     id = Column(Integer, primary_key = True)
     name = Column(String)
     surname = Column(String)
     address = Column(String)
-    lessons = relationship("lesson", secondary = person_teaches)
+    students = relationship("Student", backref = "person") #osoba je n studenty - mùže studovat více programù
+    
+    lessons = relationship("Lesson", secondary = person_teaches)
     
     def __init__(self, name, surname, address):
         self.name = name
         self.surname = surname
         self.address = address
         
-def lesson(Base):
+def Lesson(Base):
     __tablename__ = "lessons"
     
     id = Column(Integer, primary_key = True)
@@ -37,3 +42,16 @@ def lesson(Base):
     def __init__(self, date, topic):
         self.date = date
         self.topic = topic
+        
+def Student(Base):
+    __tablename__ = "students"
+    
+    id = Column(Integer, primary_key = True)
+    program_id = Column(Integer, ForeignKey("Program.id")) #jeden program má n studentù
+    person_id = Column(Integer, ForeignKey("Person.id")) #jedna osoba je n studenty
+    
+def Program(Base):
+    __tablename__ = "programs"
+    
+    id = Column(Integer, primary_key = True)
+    name = Column(String)
