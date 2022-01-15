d43b58e437db87eb4c9f2f17e316448cedb8b6c7
MrM2266 <re.marek@outlook.com>
2022-01-15 01:13:22 +0100

    application moved to docker

--- DatabaseModel/sqlalchemyCore.py
+++ DatabaseModel/sqlalchemyCore.py
@@ -3,8 +3,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Sequence
 from functools import cache
+import json
+import os
 
-DATABASE_URI = "postgresql+psycopg2://postgres:password@host.docker.internal:5432/data"
+#DATABASE_URI = "postgresql+psycopg2://root:heslo1234@uo_database:5432/data"
 
 def GetBaseSession(engineToBind):
     baseS = sessionmaker(bind = engineToBind)
@@ -18,6 +20,11 @@ def GetEngine(URI):
     newEngine = create_engine(URI)
     return newEngine
 
+def GetURI():
+    user = os.environ["POSTGRES_USER"]
+    password = os.environ["POSTGRES_PASSWORD"]
+    return "postgresql+psycopg2://" + user + ":" + password + "@database:5432/data"
+
 @cache
 def GetDeclarativeBase():
     return declarative_base()
@@ -27,6 +34,8 @@ def GetUnitedSequence(name):
     seqName = name + "_id_seq"
     unitedSequence = Sequence(seqName)
     return unitedSequence
+
+DATABASE_URI = GetURI()
 
 SQLBase = GetDeclarativeBase()
 engine = GetEngine(DATABASE_URI)
